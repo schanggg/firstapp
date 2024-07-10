77c64fc67d14447197f5d2442810ae0a51bbfd0c
marshmelody1 <msc5958@gmail.com>
2024-07-10 11:12:01 -0700

    Initial Commit

---
+++ firstapp/firstappApp.swift
@@ -0,0 +1,17 @@
+//
+//  firstappApp.swift
+//  firstapp
+//
+//  Created by Sabrina Chang on 7/10/24.
+//
+
+import SwiftUI
+
+@main
+struct firstappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
